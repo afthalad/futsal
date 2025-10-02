debfe57b9c8aac5bd43b1fcbd5c366e83af5267d
Afthal Ahmadh <afthalad8@gmail.com>
2025-10-02 12:24:02 +0530

    Add @vercel/analytics package to package.json and package-lock.json for enhanced analytics support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         {children}
         <Toaster 
           position="top-right"
