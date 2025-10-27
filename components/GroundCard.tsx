7eb83719f045b8529f73529302a4dae1a77cd301
Afthal Ahmadh <afthalad8@gmail.com>
2025-10-27 12:52:33 +0530

    Refactor GroundCard component to use Button asChild for improved link handling

--- components/GroundCard.tsx
+++ components/GroundCard.tsx
@@ -133,12 +133,11 @@ export default function GroundCard({ ground }: GroundCardProps) {
       </CardContent>
 
       <CardFooter className="p-2 sm:p-3 md:px-4 pt-0">
-        <Button className="w-full bg-primary-600 text-white hover:bg-primary-700">
-          <Link
-            href={`/grounds/${ground.id}`}
-            prefetch={true}
-            className="w-full block"
-          >
+        <Button
+          asChild
+          className="w-full bg-primary-600 text-white hover:bg-primary-700"
+        >
+          <Link href={`/grounds/${ground.id}`} prefetch={true}>
             View Details & Book
           </Link>
         </Button>
